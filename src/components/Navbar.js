46f45e94c5b201ed62f60abbbb99c6b10c2071f1
Vitālijs KOrabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2023-12-20 22:45:38 +0200

    test

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import './navBar.css';
+function Navbar(){
+    return(
+        <nav>
+            <a href="#">Item 1</a>
+            <a href="#">Item 2</a>
+            <a href="#">Item 3</a>
+            <a href="#">Item 4</a>
+            <a href="#">Item 5</a>
+        </nav>
+    )
+}
+
+export default Navbar;
